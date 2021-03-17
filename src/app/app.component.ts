7504523f7221ceca2845b09c21dd8271bd593a9a
zhoricc <giorgi.sharabidze@ugt.ge>
2021-03-18 02:43:59 +0400

    finish

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,93 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import * as epubjs from '../../node_modules/epubjs/dist/epub.min.js';
+import { DataService } from './core/data.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'epub-pwa';
+export class AppComponent implements OnInit {
+  //
+  book = null;
+
+  //
+  width = 600;
+  height = 600;
+
+  //
+  Rendition;
+
+  //
+  subscription: Subscription = new Subscription();
+
+  constructor(private dataService: DataService) {}
+
+  ngOnInit(): void {
+    this.load();
+  }
+
+  //
+  load(): void {
+    //
+    const url: string =
+      'https://zhoricc3398.github.io/ugt-csp/assets/doyle-hound-of-the-baskervilles.epub';
+
+    //
+    this.subscription.add(
+      this.dataService.getData(url).subscribe(
+        (x) => {
+          console.log(x, '--------------------------');
+
+          let firstFile = x;
+
+          if (window.FileReader) {
+            let reader: FileReader = new FileReader();
+
+            reader.onload = (e) => {
+              // console.log(e);
+              this.book = epubjs(firstFile);
+              this.Rendition = this.book.renderTo('area');
+              //getting cover
+              // this.book.coverUrl().then((result) => {
+              // document.getElementById('cover').setAttribute('src', result);
+              // });
+              this.Rendition.display();
+              /* Replace area with the id for your div to put the book in */
+            };
+
+            reader.readAsArrayBuffer(firstFile);
+
+            reader.onloadend = (e) => {
+              // console.log(book);
+            };
+          } else {
+            alert(
+              'Your browser does not support the required features. Please use a modern browser such as Google Chrome, or Mozilla Firefox'
+            );
+          }
+          // });
+        },
+        (error) => {
+          console.log(error, '-------------------------*');
+
+          // var test = new Blob(error.error.text);
+          // console.log(test);
+        }
+      )
+    );
+
+    // next page
+    document.getElementById('nextPage').addEventListener('click', () => {
+      // next page
+      this.Rendition.next();
+    });
+
+    // prev page
+    document.getElementById('prevPage').addEventListener('click', () => {
+      // prev page
+      this.Rendition.prev();
+    });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: environment.production,
+    }),
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
